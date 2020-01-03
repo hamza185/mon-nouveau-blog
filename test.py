fa81237f8e83e5790b8b0d4f67293c5a6a836ef1
hamza <hamza.miri@uit.ac.ma>
2020-01-03 21:27:01 +0100

    Modification du site

--- test.py
+++
@@ -1,8 +0,0 @@
-from sqlite3 import *
-
-conn=connect("db.sqlite3")
-c=conn.cursor()
-res=c.execute("select * ,datetime('now')-date_naiss as age from etudiant")
-for ligne in res:
-	print(ligne)
-conn.close()
